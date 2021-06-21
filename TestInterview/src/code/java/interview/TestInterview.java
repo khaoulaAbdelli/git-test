b45d3f7759d8e27bfdb4b4f497b443478ac83779
TECHNOPC <TECHNOPC@DESKTOP-ES5F57O.fritz.box>
2021-06-21 09:41:45 +0200

    squatsh-commit

--- TestInterview/src/code/java/interview/TestInterview.java
+++ TestInterview/src/code/java/interview/TestInterview.java
@@ -4,5 +4,9 @@ public class TestInterview {
 	public static void main(String[] args) {
 		String str = "KHAOULA";
 	//	chars [] test = str.toCharArray();
+		System.out.println("testst"+str);
+		System.out.println("TestGit");
+		System.out.println("TestGitCommandLine");
+		
 	}
 }
